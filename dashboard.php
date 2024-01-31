04ee3f55a239193610cc022a1815c4529f518377
Kavindu N <kavindu@digitcave.com>
2024-02-01 01:02:22 +0530

    Created Initiate
    2 UI Initial Design Concept Uploaded

---
+++ dashboard.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dashboardstyle.css">
+    <script src="https://kit.fontawesome.com/0df6f8475c.js"crossorigin="anonymous"></script>
+    <script src="dashboard.js" defer></script>
+    <title>Dashboard</title>
+</head>
+<body>
+    
+    <div class="maincontainer">
+        <div class="sidebar">
+            <img src="images/340353591_2290161711156829_779023339047059312_n.jpg" alt="" id="profile">
+            <h1>Hello</h1>
+            <h3>Kavindu</h3>
+            <div id="datetime"></div>
+
+            <div class="buttoncontainer">
+                <button class="sidebarbutton" id="employee">Employee Management</button>
+                <button class="sidebarbutton" id="stock">Stock Management</button>
+                <button class="sidebarbutton" id="Payroll">Payroll Management</button>
+                <button class="sidebarbutton" id="Settings">User Settings</button>
+                <i class="fa-solid fa-power-off logoutdashboard"><a href="">Log-Out</a></i>
+                
+
+            </div>
+        </div>
+      
+
+        <div class="content">
+            
+
+        </div>
+
+    </div>
+
+      
+
+
+
+
+
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Napronet E System</title>
+    <link rel="stylesheet" href="loginstyle.css">
+    <script src="https://kit.fontawesome.com/0df6f8475c.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <div class="container">
+        <div class="loginheader">
+            <img src="images/logo-1-192x138.png" alt="" id="logo" width="250px" height="200px">
+            <h1>Welcome</h1>
+            <h2>Napronet Pvt Ltd E-Management System</h2>
+        </div>
+
+        <div class="loginbody">
+            <form action="">
+                <div id="usericon"><i class="fa-solid fa-user"></i></div>
+                <label for="username" class="loginformlabel">Enter Your User Name</label>
+                <input type="email" name="username" id="">
+                <div class="loginpassword">
+                    <label for="password" class="loginformlabel">Enter Your Password</label>
+                    <input type="password" name="password" id="">
+                </div>
+                <button><a href="dashboard.html">Login</a></button>
+            </form>
+        </div>
+    </div>
+
+
+
+    
+</body>
+</html>
