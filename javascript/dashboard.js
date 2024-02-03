2399876c7a7e2797c274a05e6533f9c87a4e81c5
Kavindu N <kavindu@digitcave.com>
2024-02-03 12:37:54 +0530

    Modified Date and Time Config

--- javascript/dashboard.js
+++ javascript/dashboard.js
@@ -1,19 +1,27 @@
-/*Date And Time Fetching*/
 function displayDateTime() {
-    var options = {
+    var optionsDate = {
+        timeZone: 'Asia/Colombo', // Set the time zone to Sri Lanka
+        weekday: 'long', // Display the day as Monday, Tuesday, etc.
+        year: 'numeric',
+        month: 'long', // Display the month as January, February, etc.
+        day: 'numeric'
+    };
+
+    var optionsTime = {
         timeZone: 'Asia/Colombo', // Set the time zone to Sri Lanka
         hour12: true, // Use 12-hour format
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
         hour: '2-digit',
         minute: '2-digit'
     };
 
-    var currentDateTime = new Date().toLocaleString('en-US', options);
-    var [date, time] = currentDateTime.split(', ');
+    var currentDateTime = new Date();
 
+    // Display date
+    var date = currentDateTime.toLocaleDateString('en-US', optionsDate);
     document.getElementById('date').innerText = date;
+
+    // Display time
+    var time = currentDateTime.toLocaleTimeString('en-US', optionsTime);
     document.getElementById('time').innerText = time;
 }
 
@@ -22,9 +30,13 @@ window.onload = function() {
     displayDateTime();
 };
 
+
+
+
 /*menu toggle*/
 document.getElementById('sidemenu-toggle').addEventListener('click', function () {
     var sidebar = document.getElementById('sidebar');
+    var statcontainer = document.getElementById('dashstatcontainer');
 
     if (sidebar.style.display === 'none' || sidebar.style.display === '') {
         sidebar.style.display = 'block';
@@ -34,6 +46,8 @@ document.getElementById('sidemenu-toggle').addEventListener('click', function ()
     } else {
         sidebar.style.display = 'none';
         sidebar.style.width ='0%';
+        statcontainer.style.width = '100%';
+        
     }
 });
 
